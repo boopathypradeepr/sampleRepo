600878e73a5641a9deb925be57f6c5be098aa4e2
boopathypradeep <boopathy@gmail.com>
2025-03-01 17:05:11 +0530

    modified the code

--- src/test/java/com/sample/Example.java
+++ src/test/java/com/sample/Example.java
@@ -3,6 +3,7 @@ package com.sample;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,8 +11,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Example {
 @Test
 public void launchUrl() {
-//	WebDriverManager.chromedriver().setup();
-	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//driver//chromedriver");
+	WebDriverManager.chromedriver().setup();
+//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//driver//chromedriver");
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("--headless");
+	options.addArguments("--no-sandbox");
+	options.addArguments("--disable-dev-shm-usage");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://google.com/");
 	driver.quit();
