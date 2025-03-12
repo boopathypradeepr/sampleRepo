ba85bd5c64694ed5181b1524ace9af394d310976
boopathypradeepr <boopathypradeepr09@gmail.com>
2025-03-12 21:47:11 +0530

    Update Example.java

--- src/test/java/com/sample/Example.java
+++ src/test/java/com/sample/Example.java
@@ -11,9 +11,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Example {
 @Test
 public void launchUrl() {
-	// WebDriverManager.chromedriver().setup();
+	WebDriverManager.chromedriver().setup();
 //	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//driver//chromedriver.exe");
-	System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+	// System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 	// System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/JenkinsProject/testing-code-dir/driver/chromedriver");
 	ChromeOptions options=new ChromeOptions();
 	options.setBinary("/usr/bin/google-chrome");
@@ -22,12 +22,12 @@ public void launchUrl() {
 	options.addArguments("--disable-dev-shm-usage");
 	options.addArguments("--disable-gpu");
 	options.addArguments("--remote-allow-origins=*");
-	options.addArguments("remote-debugging-port=9222");
-	 options.addArguments("--disable-software-rasterizer"); // Disable software rasterizer
+	// options.addArguments("remote-debugging-port=9222");
+	//  options.addArguments("--disable-software-rasterizer"); // Disable software rasterizer
 
-        // Disable DBus if running in a headless environment
-        options.addArguments("--disable-bundled-ppapi-flash");
-        options.addArguments("--disable-plugins-discovery");
+ //        // Disable DBus if running in a headless environment
+ //        options.addArguments("--disable-bundled-ppapi-flash");
+ //        options.addArguments("--disable-plugins-discovery");
 	WebDriver driver=new ChromeDriver(options);
 	driver.get("https://google.com/");
 	driver.quit();
