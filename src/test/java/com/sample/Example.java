30b7ea1983e5b04f2ba056f918b0a1c7cc8d7b8d
boopathypradeepr <boopathypradeepr09@gmail.com>
2025-03-21 08:25:50 +0530

    Update Example.java

--- src/test/java/com/sample/Example.java
+++ src/test/java/com/sample/Example.java
@@ -11,23 +11,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Example {
 @Test
 public void launchUrl() {
-	// WebDriverManager.chromedriver().setup();
-//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//driver//chromedriver.exe");
 	System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-	// System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/JenkinsProject/testing-code-dir/driver/chromedriver");
 	ChromeOptions options=new ChromeOptions();
-	// options.setBinary("/opt/google/chrome/google-chrome");
 	options.addArguments("--headless");
 	options.addArguments("--no-sandbox");
 	options.addArguments("--disable-dev-shm-usage");
 	options.addArguments("--disable-gpu");
 	options.addArguments("--remote-allow-origins=*");
 	options.addArguments("remote-debugging-port=9222");
-	//  options.addArguments("--disable-software-rasterizer"); // Disable software rasterizer
-
- //        // Disable DBus if running in a headless environment
- //        options.addArguments("--disable-bundled-ppapi-flash");
- //        options.addArguments("--disable-plugins-discovery");
 	WebDriver driver=new ChromeDriver(options);
 	driver.get("https://google.com/");
 	driver.quit();
